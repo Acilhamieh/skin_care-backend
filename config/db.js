49aff1a1d616b85f385299988dd57036f46b7aa7
Acil <acilhamieh4@gmail.com>
2025-05-01 18:20:18 +0300

    fix the database connection

--- config/db.js
+++ config/db.js
@@ -1,23 +1,6 @@
-import pkg from 'pg';
-import dotenv from 'dotenv';
+import postgres from 'postgres'
 
-dotenv.config();
+const connectionString = process.env.DATABASE_URL
+const sql = postgres(connectionString)
 
-const { Pool } = pkg;
-
-const db = new Pool({
-  host: process.env.DB_HOSTNAME,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false, // Required for Supabase
-  },
-});
-
-db.connect()
-  .then(() => console.log('✅ Connected to PostgreSQL (Supabase) Database!'))
-  .catch((err) => console.error('❌ Connection error:', err.stack));
-
-export default db;
+export default sql
