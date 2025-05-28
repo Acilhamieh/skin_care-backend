7c4be6fc4656ca5c45193f93095ae109562ccc3f
Acil <acilhamieh4@gmail.com>
2025-05-28 11:15:42 +0300

    categories

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,15 +2,13 @@
 import express from 'express';
 import * as categoryController from '../controllers/categoryController.js';
 import upload from '../middlewares/uploadMiddleware.js';
-import { isAuthenticated, isAdmin } from '../middlewares/authMiddleware.js';
+
 
 const router = express.Router();
 
 // Add category (Admin only)
 router.post(
   '/addcategory',
-  isAuthenticated,
-  isAdmin,
   upload.fields([
     { name: 'images', maxCount: 10 },
     { name: 'baseImage', maxCount: 1 },
@@ -21,8 +19,6 @@ router.post(
 // Update a category (Admin only)
 router.put(
   '/:id',
-  isAuthenticated,
-  isAdmin,
   upload.fields([
     { name: 'baseImage', maxCount: 1 },
     { name: 'images', maxCount: 5 },
@@ -39,8 +35,6 @@ router.get('/:categoryId', categoryController.getCategoryByid);
 // Delete a category (Admin only)
 router.delete(
   '/:categoryId',
-  isAuthenticated,
-  isAdmin,
   categoryController.deleteCategoryController
 );
 
